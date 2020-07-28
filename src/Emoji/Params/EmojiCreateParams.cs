9cd352e1dfe6b91eefd4414997bf116ebeceeaf2
Yucked <yucked@protonmail.com>
2020-07-27 22:06:22 -0400

    Emoji Create and Modify params

---
+++ src/Emoji/Params/EmojiCreateParams.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Vysn.Commons;
+
+namespace Vysn.Rest.Emoji.Params {
+    /// <summary>
+    /// 
+    /// </summary>
+    public struct EmojiCreateParams {
+        /// <summary>
+        /// Name of the emoji.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The 128x128 emoji image.
+        /// </summary>
+        public object Image { get; set; }
+
+        /// <summary>
+        /// Roles to which this emoji will be whitelisted.
+        /// </summary>
+        public IReadOnlyCollection<Snowflake> Roles { get; set; }
+    }
+}
---
+++ src/Emoji/Params/EmojiModifyParams.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Vysn.Commons;
+
+namespace Vysn.Rest.Emoji.Params {
+    /// <summary>
+    /// 
+    /// </summary>
+    public struct EmojiModifyParams {
+        /// <summary>
+        /// Name of the emoji.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Roles to which this emoji will be whitelisted.
+        /// </summary>
+        public IReadOnlyCollection<Snowflake> Roles { get; set; }
+    }
+}
