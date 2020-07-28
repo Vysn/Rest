5106c5646c483a4912524fb0f1fd45601b3781a5
Yucked <yucked@protonmail.com>
2020-07-27 22:06:03 -0400

    Added AttributeUsage to RouteAttribute.cs

--- src/Attributes/RouteAttribute.cs
+++ src/Attributes/RouteAttribute.cs
@@ -2,8 +2,16 @@ using System;
 using Vysn.Rest.Attributes.Enums;
 
 namespace Vysn.Rest.Attributes {
+    [AttributeUsage(AttributeTargets.Method)]
     internal sealed class RouteAttribute : Attribute {
+        /// <summary>
+        /// 
+        /// </summary>
         public HttpType Type { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public string Path { get; }
 
         public RouteAttribute(HttpType httpType, string path) {
