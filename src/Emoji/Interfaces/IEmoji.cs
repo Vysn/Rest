2ed0051e7d81fc411920aa1ceb294030211937b3
Yucked <yucked@protonmail.com>
2020-07-27 22:06:31 -0400

    IEmoji.cs interface

---
+++ src/Emoji/Interfaces/IEmoji.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Vysn.Commons;
+using Vysn.Rest.Attributes;
+using Vysn.Rest.Attributes.Enums;
+using Vysn.Rest.Emoji.Params;
+using Vysn.Rest.Emoji.Structures;
+
+namespace Vysn.Rest.Emoji.Interfaces {
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IEmoji {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="guildId"></param>
+        /// <returns></returns>
+        [Route(HttpType.GET, "/guilds/{guildId}/emojis")]
+        Task<IReadOnlyCollection<EmojiStructure>> ListAsync(Snowflake guildId);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="guildId"></param>
+        /// <param name="emojiId"></param>
+        /// <returns></returns>
+        [Route(HttpType.GET, "/guilds/{guildId}/emojis/{emojiId}")]
+        Task<EmojiStructure> GetAsync(Snowflake guildId, Snowflake emojiId);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="guildId"></param>
+        /// <param name="createParams"></param>
+        /// <returns></returns>
+        [Route(HttpType.POST, "/guilds/{guildId}/emojis")]
+        Task<EmojiStructure> CreateAsync(Snowflake guildId, EmojiCreateParams createParams);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="guildId"></param>
+        /// <param name="emojiId"></param>
+        /// <param name="modifyParams"></param>
+        /// <returns></returns>
+        [Route(HttpType.PATCH, "/guilds/{guildId}/emojis/{emojiId}")]
+        Task<EmojiStructure> ModifyAsync(Snowflake guildId, Snowflake emojiId, EmojiModifyParams modifyParams);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="guildId"></param>
+        /// <param name="emojiId"></param>
+        /// <returns></returns>
+        [Route(HttpType.DELETE, "/guilds/{guildId}/emojis/{emojiId}")]
+        Task DeleteAsync(Snowflake guildId, Snowflake emojiId);
+    }
+}
